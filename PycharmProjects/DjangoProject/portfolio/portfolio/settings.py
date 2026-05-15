38852f53256841025a87ea1349343e4e3bc462b3
Bhanwar002 <bhanwarpreetkaur002@algomau.ca>
2026-05-15 18:23:10 -0400

    Added gitignore and removed env

--- PycharmProjects/DjangoProject/portfolio/portfolio/settings.py
+++ PycharmProjects/DjangoProject/portfolio/portfolio/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
-import smtplib
-smtplib.SMTP.timeout = 3
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,7 +149,7 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'kaurbhanwarpreet@gmail.com'
-EMAIL_HOST_PASSWORD = 'ybch rpsj yhbo pfhm'
-
-EMAIL_TIMEOUT = 3
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_TIMEOUT = 15
